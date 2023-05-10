d3c39e1d1d2ca02b598d9f1b5a05ca78b08f090a
easysnipe <tiernan.wilson@gmail.com>
2023-05-09 17:27:37 -0700

    Update Cube.java

--- 3D_Renderer/Cube.java
+++ 3D_Renderer/Cube.java
@@ -1,72 +1,25 @@
-import java.awt.*;
-import javax.swing.*;
-import java.util.concurrent.TimeUnit;
-import java.awt.event.*;
-
-public class Draw implements KeyListener
+public class Cube extends Prisim
 {
-    private JFrame win;
-    Container contentPane;
-    Graphics g;
-    Prisim shape;
-    Point d, cO, c;
-    public Draw(Prisim object) throws InterruptedException
+    public Cube(double scale)
     {
-        d = new Point(0,0,0);
-        cO = new Point(0,0,0);
-        c = new Point(0,0,0);
-        win = new JFrame("3D Renderer");
-        win.setSize(1920,1080);
-        win.setLocation(0,0);
-        win.setVisible(true);
-        contentPane = win.getContentPane();
-        g = contentPane.getGraphics();
-        win.addKeyListener(this);
-        shape = object;
-        TimeUnit.MILLISECONDS.sleep(300);
-        
+        super(scale);
+        points = new Point[8];
+        edges = new Line[12];
     }
-    public void keyPressed(KeyEvent e)
+    public void create3dPoints(int midX, int midY)
     {
-        System.out.println(e.getKeyChar());
-        if (e.getKeyChar() == 'a')
+        points[0] = new Point(0,0,0); 
+        points[1] = new Point(0,0,scale); 
+        points[2] = new Point(scale,0,0); 
+        points[3] = new Point(scale,0,scale); 
+        points[4] = new Point(scale,scale,0); 
+        points[5] = new Point(scale,scale,scale); 
+        points[6] = new Point(0,scale,scale); 
+        points[7] = new Point(0,scale,0);
+        for (Point point: points)
         {
-            d.setZ3(d.getZ3() + 1);
+            point.setX3(point.getX3() + midX);
+            point.+
         } 
-        else if (e.getKeyChar() == 's')
-        {
-            d.setZ3(d.getZ3() - 1);
-        }
-        DrawShapes(d,cO,c);
     }
-    public void keyReleased(KeyEvent e)
-    {
-        //System.out.println("Key Pressed");
-    }
-    public void keyTyped(KeyEvent e)
-    {
-        //System.out.println("Key Pressed");
-
-    }
-    public void DrawShapes(Point display, Point camOrientation, Point cam)
-    {
-        shape.create3dPoints(1920/2, 1080/2);
-        d = display;
-        cO = camOrientation;
-        c = cam;
-        shape.CreatePerspective(c, cO);
-        shape.ConvertTo2D(d);
-        shape.createLines();
-        Line[] lines = shape.getLines();
-        System.out.println(lines[0].getPointA().getX2());
-        g.setColor(new Color(0,0,0));
-        for (Point P : shape.getPoints())
-        {
-            System.out.println("(" + P.getX2() + " ," + P.getY2() + ")");
-        }
-        for (Line line : lines)
-        {
-            g.drawLine((int)line.getPointA().getX2(), (int)line.getPointA().getY2(), (int)line.getPointB().getX2(), (int)line.getPointB().getY2());
-        }
-    }
-}   
+}
